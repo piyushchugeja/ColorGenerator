3b18e11208271779ba4d972501133c24b982dc37
Piyush Chugeja <piyushchugeja@gmail.com>
2021-10-02 18:56:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+//base Hexadecimal
 var characters = "0123456789ABCDEF";
+
 const body = document.querySelector("body");
 const colorBox = document.querySelector("#color");
+
+//Call main function when page is loaded
 document.ready = generateColor();
+
+//define main function
 function generateColor() {
     var color = "#";
     for (var i = 0; i < 6; i++) {
@@ -11,10 +17,13 @@ function generateColor() {
     colorBox.value = color;
     document.getElementById("tooltip").innerHTML = "Click to copy to clipboard.";
 }
+
+//define function used to copy the color code
 function copyColor() {
     colorBox.select();
     navigator.clipboard.writeText(colorBox.value);
     document.getElementById("tooltip").innerHTML = "Copied!";
 }
+
 colorBox.addEventListener("click", copyColor);
 document.getElementById("generator").addEventListener("click", generateColor);
